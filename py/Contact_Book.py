d87cfced24b3f7635afa8987b8006199b2f1267a
Dillan Kerai <dillankerai3@outlook.com>
2021-07-16 11:30:52 +0100

    Cleaning Code: Contact_Book.py

--- py/Contact_Book.py
+++ py/Contact_Book.py
@@ -29,20 +29,13 @@ scroll = tk.Scrollbar(frame, orient=tk.HORIZONTAL)
 yscroll = tk.Scrollbar(frame, orient=tk.VERTICAL)
 
 lt = tk.Listbox(frame, xscrollcommand=scroll.set, yscrollcommand=yscroll.set, height=16, width=50)
-# lt.place(relx = 9, rely = 778, anchor="center")
 
-# scroll.config(command=list.yview)
 scroll.config(command=lt.xview)
-# scroll.pack(side=tk.RIGHT)
 scroll.pack(side = tk.BOTTOM)
 
-
-# scroll.config(command=list.yview)
 yscroll.config(command=lt.yview)
-# scroll.pack(side=tk.RIGHT)
 yscroll.pack(side = tk.RIGHT)
 
-# lt.place(relx = 9, rely = 778, anchor="center")
 lt.pack()
 
 contacts = [
@@ -61,7 +54,6 @@ def select_index():
     return int(lt.curselection()[0])
 
 #Clear
-
 def clear():
     name.set("")
     phonenum.set("")
@@ -69,20 +61,8 @@ def clear():
     
 #Save Info
 def save():
-    # global holder
-
-    # if holder != -1:
-    #     del contacts[holder]
-    #     # print(select_index())
-    #     # delete()
-    #     # contacts[value] = [name.get(), phonenum.get(),email.get()]
-
-        contacts.append([name.get(),phonenum.get(),email.get()])
-        refresh()
-    # else:
-
-    #     contacts.append([name.get(),phonenum.get(),email.get()])
-    #     refresh()
+    contacts.append([name.get(),phonenum.get(),email.get()])
+    refresh()
 
 #Exit
 def exit():
@@ -90,24 +70,13 @@ def exit():
 
 #Edit
 def edit():
-    # global holder
-    # # contacts[select_index()] = [name.get(), phonenum.get(),email.get()]
-
-    # n , p , e = contacts[select_index()]
-    # name.set(n)
-    # phonenum.set(p)
-    # email.set(e)
-    # holder = int(lt.curselection()[0])
-    # # delete()
-    # # refresh()
     del contacts[select_index()]
     contacts[select_index()] = [name.get(), phonenum.get(),email.get()]
-
     refresh()
+
 #Delete
 def delete():
     del contacts[select_index()]
-
     refresh()
 
 #Refresh - Update the ListBox
@@ -115,9 +84,7 @@ def refresh():
     lt.delete(0,tk.END)
     contacts.sort()
     for Name,PhoneNumber,Email in contacts:
-    # print(Name,PhoneNumber,Email)
         lt.insert(tk.END,Name+'   '+PhoneNumber+'   '+Email)
-
 
 #Buttons
 tk.Button(window,text="NEW",width = 8, height = 3,bg ='white' ,highlightthickness=1, highlightbackground="black",command = clear).place(x=26,y=200)
@@ -127,11 +94,7 @@ tk.Button(window,text="DELETE",width = 8, height = 3,bg ='white' ,highlightthick
 tk.Button(window,text="EXIT",width = 8, height = 3,bg ='white' ,highlightthickness=1, highlightbackground="black", command = exit).place(x=26,y=440)
 
 
-
-
-#File Access
-
 for Name,PhoneNumber,Email in contacts:
-    # print(Name,PhoneNumber,Email)
     lt.insert(tk.END,Name+'   '+PhoneNumber+'   '+Email)
+    
 window.mainloop()
