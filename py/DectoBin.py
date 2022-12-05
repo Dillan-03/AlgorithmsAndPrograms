d8232c7dbef9fb6f1bb4ed47a8ac09fe9d5e7d01
Dillan Kerai <sc21dmk@leeds.ac.uk>
2022-12-05 14:30:43 +0000

    correction

--- py/DectoBin.py
+++ py/DectoBin.py
@@ -1,13 +1,14 @@
 #Program which converts decimal to binary representation through recursion
 import math
 
-holder = []  
+holder = []
 def dectobin(x,holder):
 
 	if x > 0:
-		holder.append(str(x%2))
+		holder.append(str((x%2)))
 		dectobin(math.floor(x/2),holder)
-	return ''.join(holder)
+	temp = holder[::-1]
+	return ''.join(temp)
 
 var = dectobin(13,holder)
 
